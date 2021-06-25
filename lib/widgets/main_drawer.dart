50048b5126d2dc92c73f89b1558ef5af91556a26
alrawas <alrawas.abed@gmail.com>
2021-06-26 00:00:17 +0300

    first commit - tabs

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 120,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
